702e1e2fa1bd5799178150f946a16b3f9728ad2b
Olivier LIN-SI-CHENG <olivier.lin-si-cheng>
2017-10-25 14:23:11 +0200

    use service in testing

--- testing/src/app/app.component.ts
+++ testing/src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import {Inject} from "../../../node_modules/@angular/core/src/di/metadata";
+import { Component, OnDestroy, Inject, OnInit } from '@angular/core';
 import {PageVisibilityService} from "../../../src/angular-page-visibility";
+import {Subscription} from "rxjs/Subscription";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'app';
+@Component( {
+    selector : 'app-root',
+    templateUrl : './app.component.html',
+    styleUrls : [ './app.component.scss' ]
+} )
+export class AppComponent implements OnDestroy, OnInit {
+    private onPageVisibleSubscription: Subscription;
+    private onPageNotVisibleSubscription: Subscription;
+    title = 'app';
 
-  constructor(private pageVisibilityService: PageVisibilityService){
-    this.pageVisibilityService.$onPageVisible.subscribe(()=>{
-      console.log('visible');
-    });
+    constructor( private pageVisibilityService: PageVisibilityService ) {
+        this.onPageVisibleSubscription = this.pageVisibilityService.$onPageVisible.subscribe( ()=> {
+            console.log( 'visible' );
+        } );
 
-    this.pageVisibilityService.$onPageNotVisible.subscribe(()=>{
-      console.log('notVisible');
-    });
-  }
+        this.onPageNotVisibleSubscription = this.pageVisibilityService.$onPageNotVisible.subscribe( ()=> {
+            console.log( 'notVisible' );
+        } );
+    }
+
+    ngOnInit(): void {
+        if ( this.pageVisibilityService.isPageVisible() ) {
+            console.log( 'visible' );
+        }
+        if ( this.pageVisibilityService.isPageNotVisible() ) {
+            console.log( 'notVisible' );
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.onPageVisibleSubscription.unsubscribe();
+        this.onPageNotVisibleSubscription.unsubscribe();
+    }
 }
