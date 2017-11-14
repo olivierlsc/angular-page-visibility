0709147543060597dedad40bc9d5eec5ac155d0b
Olivier LIN-SI-CHENG <olivier.lin-si-cheng>
2017-11-14 11:26:07 +0100

    fix bug

--- src/angular-page-visibility.ts
+++ src/angular-page-visibility.ts
@@ -1,5 +1,3 @@
 export {AngularPageVisibilityModule} from './angular-page-visibility.module';
 export {PageVisibilityService} from './page-visibility.service';
-export {OnPageVisible} from './decorators/on-page-visible.decorator';
-export {OnPageNotVisible} from './decorators/on-page-not-visible.decorator';
-export {OnPageVisibilityChange} from './decorators/on-page-visibility-change.decorator';
+export {OnPageVisibilityChange, OnPageVisible, OnPageNotVisible} from './decorators';
---
+++ src/decorators.ts
@@ -0,0 +1,31 @@
+import {PageVisibilityService} from './page-visibility.service';
+import {AngularPageVisibilityModule} from './angular-page-visibility.module';
+import { ReflectiveInjector, Provider } from '@angular/core';
+
+let providers: Provider[] = [ PageVisibilityService ];
+const injector = ReflectiveInjector.resolveAndCreate( providers );
+const pageVisibilityService = injector.get( PageVisibilityService );
+
+export function OnPageVisibilityChange(): MethodDecorator {
+    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        pageVisibilityService.$onPageVisibilityChange.subscribe( ( isPageVisible: boolean )=> {
+            target[ propertyKey ].apply( this, [ isPageVisible ] );
+        } );
+    };
+}
+
+export function OnPageNotVisible(): MethodDecorator {
+    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        pageVisibilityService.$onPageNotVisible.subscribe( ()=> {
+            target[ propertyKey ].apply( this );
+        } );
+    };
+}
+
+export function OnPageVisible(): MethodDecorator {
+    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        pageVisibilityService.$onPageVisible.subscribe( ()=> {
+            target[ propertyKey ].apply( this );
+        } );
+    };
+}
