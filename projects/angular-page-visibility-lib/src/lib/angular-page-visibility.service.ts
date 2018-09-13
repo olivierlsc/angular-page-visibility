13ec763392404664eeb41df28236fdaf65af265d
Olivier LIN-SI-CHENG <olivier.lin-si-cheng>
2018-09-13 16:40:08 +0200

    refactor lib

--- projects/angular-page-visibility-lib/src/lib/angular-page-visibility.service.ts
+++ projects/angular-page-visibility-lib/src/lib/angular-page-visibility.service.ts
@@ -10,48 +10,31 @@ export class AngularPageVisibilityService {
   private static MS_HIDDEN = 'msHidden';
   private static WEB_KIT_HIDDEN = 'webkitHidden';
   private onPageVisibleSource: Subject<void> = new Subject<void> ();
-  /**
-   * @deprecated from v4.0.9 use isPageHidden(): void
-   */
-  private onPageNotVisibleSource: Subject<void> = new Subject<void> ();
   private onPageHiddenSource: Subject<void> = new Subject<void> ();
   private onPageVisibilityChangeSource: Subject<boolean> = new Subject<boolean> ();
   private hidden: string;
   private visibilityChange: string;
   $onPageVisible: Observable<void> = this.onPageVisibleSource.asObservable();
-  /**
-   * @deprecated from v4.0.9 use $onPageHidden: Observable<void>
-   */
-  $onPageNotVisible: Observable<void> = this.onPageNotVisibleSource.asObservable();
   $onPageHidden: Observable<void> = this.onPageHiddenSource.asObservable();
   $onPageVisibilityChange: Observable<boolean> = this.onPageVisibilityChangeSource.asObservable();
 
   constructor () {
-    this.init();
     this.listenPageVisibility();
   }
 
   isPageVisible (): boolean {
     return ! this.isPageHidden();
-  };
+  }
 
   isPageHidden (): boolean {
     return document[ this.hidden ];
   }
 
-  /**
-   * @deprecated from v4.0.9 use isPageHidden(): void
-   */
-  isPageNotVisible (): boolean {
-    return this.isPageHidden();
-  }
-
-  private init () {
+  private defineBrowserSupport () {
     if ( typeof document[ AngularPageVisibilityService.HIDDEN ] !== 'undefined' ) { // Opera 12.10 and Firefox 18 and later support
       this.hidden = AngularPageVisibilityService.HIDDEN;
       this.visibilityChange = 'visibilitychange';
-    }
-    else if ( typeof document[ AngularPageVisibilityService.MS_HIDDEN ] !== 'undefined' ) {
+    } else if ( typeof document[ AngularPageVisibilityService.MS_HIDDEN ] !== 'undefined' ) {
       this.hidden = AngularPageVisibilityService.MS_HIDDEN;
       this.visibilityChange = 'msvisibilitychange';
     } else if ( typeof document[ AngularPageVisibilityService.WEB_KIT_HIDDEN ] !== 'undefined' ) {
@@ -61,6 +44,7 @@ export class AngularPageVisibilityService {
   }
 
   private listenPageVisibility (): void {
+    this.defineBrowserSupport();
     document.addEventListener( this.visibilityChange , () => {
       if ( this.isPageVisible() ) {
         this.onPageVisibilityChangeSource.next( true );
@@ -69,7 +53,6 @@ export class AngularPageVisibilityService {
       }
       this.onPageVisibilityChangeSource.next( false );
       this.onPageHiddenSource.next();
-      this.onPageNotVisibleSource.next();
     } , false );
   }
 }
